c61c72b15f5d54071c03677d9b7704fbf1f4cfc5
harshmangalam <harshdev8218@gmail.com>
2022-09-19 10:49:04 +0530

    added auth handler

--- handlers/auth.go
+++ handlers/auth.go
@@ -6,18 +6,40 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func AuthLogin(c echo.Context) error {
+type AuthHandler struct {
+}
+
+func NewAuthHandler() *AuthHandler {
+	return &AuthHandler{}
+}
+
+func (h *AuthHandler) AuthLogin(c echo.Context) error {
 	return c.String(http.StatusOK, "Login")
 }
 
-func AuthSignup(c echo.Context) error {
-	return c.String(http.StatusCreated, "Signup")
+func (h *AuthHandler) AuthSignup(c echo.Context) error {
+	// parse request body in form of struct
+	type SignupData struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	signupData := new(SignupData)
+
+	// bind incomming request body to  struct
+	if err := c.Bind(signupData); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	//
+	return c.JSON(http.StatusCreated, signupData)
 }
 
-func AuthLogout(c echo.Context) error {
+func (h *AuthHandler) AuthLogout(c echo.Context) error {
 	return c.String(http.StatusOK, "Logout")
 }
 
-func AuthMe(c echo.Context) error {
+func (h *AuthHandler) AuthMe(c echo.Context) error {
 	return c.String(http.StatusOK, "Me")
 }
--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"socialmedia/handlers"
+
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	authHandler := handlers.NewAuthHandler()
 
+	// auth group routes
 	auth := e.Group("/auth")
-
-	auth.POST("/login", func(c echo.Context) error {})
-	auth.POST("/signup", func(c echo.Context) error {})
-	auth.GET("/logout", func(c echo.Context) error {})
-	auth.GET("/me", func(c echo.Context) error {})
+	auth.POST("/login", authHandler.AuthLogin)
+	auth.POST("/signup", authHandler.AuthSignup)
+	auth.GET("/me", authHandler.AuthMe)
+	auth.GET("/logout", authHandler.AuthLogout)
 
 	e.Logger.Fatal(e.Start(":4000"))
 }
