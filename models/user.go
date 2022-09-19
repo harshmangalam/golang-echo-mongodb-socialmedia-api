53796d4733cbd99fc1236f12d11e7f9252d3f782
harshmangalam <harshdev8218@gmail.com>
2022-09-19 15:59:26 +0530

    updated user id field

--- models/user.go
+++ models/user.go
@@ -2,15 +2,13 @@ package models
 
 import (
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	Id        primitive.ObjectID `json:"_id" bson:"_id"`
-	Name      string             `json:"name" bson:"name"`
-	Email     string             `json:"email" bson:"email"`
-	Password  string             `json:"password,omitempty" bson:"password,omitempty"`
-	CreatedAt time.Time          `json:"createdAt" bson:"createdAt"`
-	UpdatedAt time.Time          `json:"updatedAt" bson:"updatedAt"`
+	Id        string    `json:"_id" bson:"_id"`
+	Name      string    `json:"name" bson:"name"`
+	Email     string    `json:"email" bson:"email"`
+	Password  string    `json:"password,omitempty" bson:"password,omitempty"`
+	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
 }
