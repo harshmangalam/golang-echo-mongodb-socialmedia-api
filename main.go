41497860388b8141ade1ff03b43d2bbfcc0f68e3
harshmangalam <harshdev8218@gmail.com>
2022-09-19 15:59:10 +0530

    added jwt config middleware

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"socialmedia/handlers"
+	"socialmedia/utils"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -36,6 +39,21 @@ func main() {
 	auth.POST("/signup", authHandler.AuthSignup)
 	auth.GET("/me", authHandler.AuthMe)
 	auth.GET("/logout", authHandler.AuthLogout)
+
+	// post group
+
+	postHandler := handlers.NewPostHandler(db)
+
+	post := e.Group("/posts")
+	post.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningKey:     []byte("itssecret"),
+		ParseTokenFunc: utils.ParseTokenFunc,
+		SuccessHandler: func(c echo.Context) {
+			fmt.Println(c.Get("userId"))
+		},
+		ContextKey: "userId",
+	}))
+	post.GET("/", postHandler.GetPosts)
 
 	e.Logger.Fatal(e.Start(":4000"))
 }
