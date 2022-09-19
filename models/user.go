b14d159ba656e570bf105c4ca10978f882b7ecc8
harshmangalam <harshdev8218@gmail.com>
2022-09-19 11:48:56 +0530

    updated user models

--- models/user.go
+++ models/user.go
@@ -7,10 +7,10 @@ import (
 )
 
 type User struct {
-	Id        primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+	Id        primitive.ObjectID `json:"_id" bson:"_id"`
 	Name      string             `json:"name" bson:"name"`
 	Email     string             `json:"email" bson:"email"`
-	Password  string             `json:"password" bson:"password"`
+	Password  string             `json:"password,omitempty" bson:"password,omitempty"`
 	CreatedAt time.Time          `json:"createdAt" bson:"createdAt"`
 	UpdatedAt time.Time          `json:"updatedAt" bson:"updatedAt"`
 }
