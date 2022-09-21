d63a8a47ab299e21e80e69c419128d9e0c73fcac
harshmangalam <harshdev8218@gmail.com>
2022-09-21 22:08:27 +0530

    added active fields

--- models/user.go
+++ models/user.go
@@ -8,6 +8,7 @@ type User struct {
 	Id        string    `json:"_id" bson:"_id"`
 	Name      string    `json:"name" bson:"name"`
 	Email     string    `json:"email" bson:"email"`
+	IsActive  bool      `json:"isActive" bson:"isActive"`
 	Password  string    `json:"password,omitempty" bson:"password,omitempty"`
 	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
